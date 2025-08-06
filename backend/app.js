d7e7441822818bb223690ec9be5236df02f5bf6e
Anuj Yadav <anujyadav47247@gmail.com>
2025-08-06 11:43:45 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,9 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/v1/reservation", reservationRouter);
 
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Welcome to Zestora API" });
+});
 // DB connection
 dbConnection();
 
